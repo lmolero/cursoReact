13c8922b3a471ac02171f067fc4564cbebd5bf86
Lisany Molero <lisanymoleromachado@gmail.com>
2024-06-15 21:37:47 +0200

    Terminamos de aplicar el CSS  a los componentes main y navbar para dejar el index como el ejemplo del curso

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 
